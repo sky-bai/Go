cd1869c8cabf90a6a461c96c37e9db07600feebe
sky-bai <52771070+sky-bai@users.noreply.github.com>
2020-01-19 15:35:33 +0800

    Add files via upload

---
+++ channel.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//channel 相当于一条管道 一边装水 一边取水
+//默认状态下是阻塞的
+
+//功能：1同步  有先有后
+// 	   2实现数据交互
+
+
+
+
+
+func main()  {
+	go func() {
+		defer fmt.Println("子协程调用完毕")
+
+		for i := 0; i < 2; i++ {
+			fmt.Println(i)
+			time.Sleep(time.Second)
+		}
+	}()
+}
+
+
+//此时什么现象也不会出现 main协程就执行完了 go func协程不会执行
